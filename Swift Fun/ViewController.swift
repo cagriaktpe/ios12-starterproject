3861ee29a67cbe715eb105acb3d509d79df53d7d
Zappy Code <madeupbypeople@gmail.com>
2018-06-13 13:48:59 +0900

    Initial Commit

---
+++ Swift Fun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift Fun
+//
+//  Created by zappycode on 6/13/18.
+//  Copyright © 2018 Nick Walter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
