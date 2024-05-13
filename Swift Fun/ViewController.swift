f57dc841a1e965a1910f8218ec5e7d09ce2ad0a5
Samet cagri Aktepe <sametcagriaktepe@gmail.com>
2024-05-13 13:12:44 +0300

    Update

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var myFirstLabel: UILabel!
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
-
-
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        count = count + 1
+        myFirstLabel.text = String(count)
+        
+        if count >= 10 {
+            view.backgroundColor = UIColor.green
+        }
+    }
+    
 }
 
